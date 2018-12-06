a65421b957f0be259f3602bc43d7b26e9c22a9a4
Abhijeetholkar97 <41098357+Abhijeetholkar97@users.noreply.github.com>
2018-12-06 10:59:38 +0530

    Add files via upload

---
+++ increasing numerical right angled triangle.py
@@ -0,0 +1,16 @@
+i=1
+#i have a variable i and  ahve given it a value as 1
+while i<=4:
+	#here i have a condition while/till value of i is less than or equal to 4
+    a=1
+    #here i have another variable as a and its value as 1
+    while a<=i:            
+    #here i have another condition while/till a is less than or equal to value of i
+        print (i, end=" ")
+        #here the value of i is being printed with space in the end
+        a=a+1
+        #this is my counter
+    print (" ")
+    #here im again printing space
+    i=i+1
+    # this is my another counter
